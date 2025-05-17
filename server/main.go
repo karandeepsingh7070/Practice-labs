bb3611272e26482aa8ef2be380a1571e007e091c
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-05-17 14:52:46 +0530

    Auth API

--- server/main.go
+++ server/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type User struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
+
+var authSecret = []byte("copowered by WisdomBits")
 
 func main() {
 	app := fiber.New()
@@ -22,6 +27,34 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("You are connected to the Practice Labs Server")
 	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		user := new(User)
+		if err := c.BodyParser(user); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid request",
+			})
+		}
+		claims := jwt.MapClaims{
+			"name":  user.Name,
+			"email": user.Email,
+			"exp":   time.Now().Add(time.Hour * 24).Unix(), // expires in 1 day
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		signedToken, err := token.SignedString(authSecret)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Could not generate token",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "Success",
+			"user":    user,
+			"token":   signedToken,
+		})
+	})
+
 	println("🚀 Server running at http://localhost:8000")
 	err := app.Listen(":8000")
 	if err != nil {
